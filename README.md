a97803aadb566725fe79320805e52b0eb2a38157
Balthasar Schwegler <balu@Balthasars-MacBook-Air-3.local>
2020-04-29 18:01:12 +0200

    init the vuejs application using vue-cli

--- README.md
+++
@@ -1,2 +0,0 @@
-# tab-tracker
-A VueJS /ExpressJS app
---
+++ client/src/components/Register.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Register</v-toolbar-title>
+                <v-spacer />
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn :href="source" icon large target="_blank" v-on="on">
+                      <v-icon>mdi-code-tags</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      icon
+                      large
+                      href="https://codepen.io/johnjleider/pen/pMvGQO"
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-codepen</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Codepen</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <span class="group pa-2">
+                    <v-icon class="icon">mdi-account</v-icon>
+                    <v-text-field
+                      label="email"
+                      name="login"
+                      type="email"
+                      v-model="email"
+                    />
+                  </span>
+                  <span class="group pa-2">
+                    <v-icon class="icon">mdi-lock</v-icon>
+                    <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      type="password"
+                      v-model="password"
+                    />
+                  </span>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style>
+.group {
+  display: flex;
+  flex: 1;
+  justify-content: space-around;
+}
+</style>
